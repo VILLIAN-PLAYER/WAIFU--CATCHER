188066d72634a00c7d2624d2b62a45d3c6b5095c
Toxic Boi <138990052+vil3n3@users.noreply.github.com>
2024-02-20 08:05:24 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -8,7 +8,7 @@ if env:
     sudo_users = list(map(int, os.environ.get("1813373023", "").split()))
     GROUP_ID = os.environ.get("-1002103727164")
     TOKEN = os.environ.get("7191385539:AAHwhpupUBixxB7wehd5PR2NWaUawVz9l3A")
-    mongo_url = os.environ.get("mongodb+srv://hnyx:wywyw2@cluster0.9dxlslv.mongodb.net/?retryWrites=true&w=majority")
+    mongo_url = os.environ.get("mongodb+srv://Villan:villan654@villen.ifsk8q0.mongodb.net/?retryWrites=true&w=majority")
     if os.environ.get("https://telegra.ph/file/a0a18f4619fed4b45ea01.jpg") != None:
         PHOTO_URL = os.environ.get("https://telegra.ph/file/a0a18f4619fed4b45ea01.jpg") 
     else:
@@ -23,12 +23,12 @@ else:
     OWNER_ID = 1813373023
     sudo_users = ["1813373023"]
     GROUP_ID = -1002103727164
-    mongo_url = "mongodb+srv://hnyx:wywyw2@cluster0.9dxlslv.mongodb.net/?retryWrites=true&w=majority"    
+    mongo_url = "mongodb+srv://Villan:villan654@villen.ifsk8q0.mongodb.net/?retryWrites=true&w=majority"    
     TOKEN = "7191385539:AAHwhpupUBixxB7wehd5PR2NWaUawVz9l3A"
     PHOTO_URL = ["https://telegra.ph/file/a0a18f4619fed4b45ea01.jpg", "https://telegra.ph/file/a0a18f4619fed4b45ea01.jpg"]
     SUPPORT_CHAT = "FallenXDeveloper"
     UPDATE_CHAT = "FallenXDeveloper"
     BOT_USERNAME = "Keqing_WaifuBot"
     CHARA_CHANNEL_ID = -1002074617147
-    api_id = 21846639
+    api_id = 21846639q
     api_hash = "2cebc99bd8378b5237b31ea8e7496d79"
